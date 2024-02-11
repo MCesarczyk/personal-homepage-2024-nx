64fc5b388a5d2ceaefa23048eaa8cf2b40e92570
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2024-02-11 16:31:02 +0100

    fix: update styles (#10)
    * chore(tailwind): add to ui library, configure for proper builds
    
    * chore(tailwind): craete config for spa frontend
    
    * feat(theme): add theme changing feature in Tailwind, migrate Caption and Link components
    
    * feat(tailwind): migrate all components
    
    * feat(tailwind): finish migration, update theme management, uninstall styled-components
    
    * chore: remove all styled-components references
    
    * fix: adjust styles
    
    * fix: update styles

--- libs/ui/src/organisms/themeSwitcher/ThemeSwitcher.tsx
+++ libs/ui/src/organisms/themeSwitcher/ThemeSwitcher.tsx
@@ -17,10 +17,10 @@ export const ThemeSwitcher = ({
         className="flex items-center justify-end gap-3 text-xs h-6 p-0 border-none bg-transparent"
         onClick={toggleDarkTheme}
       >
-        <span className="hidden sm:block text-inherit">
+        <span className="hidden sm:block">
           {`${isDarkTheme ? 'DARK' : 'LIGHT'}`}&nbsp;MODE&nbsp;ON
         </span>
-        <div className="flex justify-start border-2 border-inherit rounded-2xl w-16 h-8">
+        <div className="flex justify-start border-2 border-current rounded-2xl w-16 h-8">
           <div
             className={clsx(
               'w-1/2 transition-all duration-300',
