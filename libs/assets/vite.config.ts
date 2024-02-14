c2dc3f0d2f4210bbb0a11abd6fa5a81f1b9f07c0
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2024-02-14 23:30:17 +0100

    build: update projects configs

--- libs/assets/vite.config.ts
+++ libs/assets/vite.config.ts
@@ -1,13 +1,85 @@
-/// <reference types='vitest' />
+// /// <reference types='vitest' />
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import dts from 'vite-plugin-dts';
+// import * as path from 'path';
+// import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+// export default defineConfig({
+//   root: __dirname,
+//   cacheDir: '../../node_modules/.vite/libs/assets',
+
+//   server: {
+//     port: 4200,
+//     host: 'localhost',
+//   },
+
+//   preview: {
+//     port: 4300,
+//     host: 'localhost',
+//   },
+
+//   plugins: [
+//     react(),
+//     nxViteTsPaths(),
+//     dts({
+//       entryRoot: 'src',
+//       tsConfigFilePath: path.join(__dirname, 'tsconfig.lib.json'),
+//       skipDiagnostics: true,
+//     }),
+//   ],
+
+//   // Uncomment this if you are using workers.
+//   // worker: {
+//   //  plugins: [ nxViteTsPaths() ],
+//   // },
+
+//   // Configuration for building your library.
+//   // See: https://vitejs.dev/guide/build.html#library-mode
+//   build: {
+//     outDir: '../../dist/libs/assets',
+//     reportCompressedSize: true,
+//     commonjsOptions: {
+//       transformMixedEsModules: true,
+//     },
+//     lib: {
+//       // Could also be a dictionary or array of multiple entry points.
+//       entry: 'src/index.ts',
+//       name: 'assets',
+//       fileName: 'index',
+//       // Change this to the formats you want to support.
+//       // Don't forget to update your package.json as well.
+//       formats: ['es', 'cjs'],
+//     },
+//     rollupOptions: {
+//       // External packages that should not be bundled into your library.
+//       external: ['react', 'react-dom', 'react/jsx-runtime'],
+//     },
+//   },
+
+//   test: {
+//     globals: true,
+//     cache: {
+//       dir: '../../node_modules/.vitest',
+//     },
+//     environment: 'jsdom',
+//     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+
+//     reporters: ['default'],
+//     coverage: {
+//       reportsDirectory: '../../coverage/libs/assets',
+//       provider: 'v8',
+//     },
+//   },
+// });
+
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dts from 'vite-plugin-dts';
-import * as path from 'path';
-import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  root: __dirname,
-  cacheDir: '../../node_modules/.vite/libs/assets',
+  cacheDir: '../../node_modules/.vite/frontend',
 
   server: {
     port: 4200,
@@ -21,41 +93,19 @@ export default defineConfig({
 
   plugins: [
     react(),
-    nxViteTsPaths(),
-    dts({
-      entryRoot: 'src',
-      tsConfigFilePath: path.join(__dirname, 'tsconfig.lib.json'),
-      skipDiagnostics: true,
+    viteTsConfigPaths({
+      root: '../../',
     }),
   ],
 
   // Uncomment this if you are using workers.
   // worker: {
-  //  plugins: [ nxViteTsPaths() ],
+  //  plugins: [
+  //    viteTsConfigPaths({
+  //      root: '../../',
+  //    }),
+  //  ],
   // },
-
-  // Configuration for building your library.
-  // See: https://vitejs.dev/guide/build.html#library-mode
-  build: {
-    outDir: '../../dist/libs/assets',
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-    lib: {
-      // Could also be a dictionary or array of multiple entry points.
-      entry: 'src/index.ts',
-      name: 'assets',
-      fileName: 'index',
-      // Change this to the formats you want to support.
-      // Don't forget to update your package.json as well.
-      formats: ['es', 'cjs'],
-    },
-    rollupOptions: {
-      // External packages that should not be bundled into your library.
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
-    },
-  },
 
   test: {
     globals: true,
@@ -64,11 +114,5 @@ export default defineConfig({
     },
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-
-    reporters: ['default'],
-    coverage: {
-      reportsDirectory: '../../coverage/libs/assets',
-      provider: 'v8',
-    },
   },
 });
