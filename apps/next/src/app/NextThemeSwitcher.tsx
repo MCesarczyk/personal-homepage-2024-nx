72a3cb77fbf02991fdb8f52e62f05e21741e771a
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2024-02-14 01:54:03 +0100

    fix(next): temporarily disable savong theme in local storage
    restore after finding solution to handle local storage after client component mount

--- apps/next/src/app/NextThemeSwitcher.tsx
+++ apps/next/src/app/NextThemeSwitcher.tsx
@@ -3,17 +3,17 @@
 import { useEffect, useState } from 'react';
 
 import { ThemeSwitcher } from '@ui';
-import { Theme } from '@types';
-import { localStorageService } from '@services';
+// import { Theme } from '@types';
+// import { localStorageService } from '@services';
 
 export const NextThemeSwitcher = () => {
-  const initialTheme = localStorageService.getValue('theme', 'light' as Theme);
-  const [isDarkTheme, setDarkTheme] = useState(initialTheme === 'dark');
+  // const initialTheme = localStorageService.getValue('theme', 'light' as Theme); // FIXME: Restore saving in local storage after fixing the issue with the server-side rendering
+  const [isDarkTheme, setDarkTheme] = useState(true);
 
   const onThemeChange = () => {
     setDarkTheme(!isDarkTheme);
 
-    localStorageService.setValue('theme', !isDarkTheme ? 'dark' : 'light');
+    // localStorageService.setValue('theme', !isDarkTheme ? 'dark' : 'light');
   };
 
   useEffect(() => {
