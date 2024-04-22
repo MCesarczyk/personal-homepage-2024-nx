049efaac3f69a2cbb1c8e485a3be66617bcbfc3c
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-04-22 02:41:11 +0200

    docs(frontpage): update docs url

--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
+const port = process.env.NODE_ENV === 'production' ? process.env.PRODUCTION_URL : process.env.PORT;
+
 @Injectable()
 export class AppService {
   getHello(): string {
-    return process.env.NODE_ENV === 'production'
-      ? '<h1>Backend for <a href="https://cesarczyk.dev" >cesarczyk.dev</a> portfolio up and running 🚀</h1>\
-    <p><h3>Docs available at <a href="https://personal-homepage-2024-backend-production.up.railway.app/docs" >https://personal-homepage-2024-backend-production.up.railway.app/docs</a></h3></p>'
-      : `<h1>Backend for <a href="https://cesarczyk.dev" >cesarczyk.dev</a> portfolio up and running 🚀</h1>\
-    <p><h3>Docs available at <a href="http://localhost:${process.env.PORT}/docs" >http://localhost:${process.env.PORT}/docs</a></h3></p>`;
+    return `<h1>Backend for <a href="https://cesarczyk.dev" >cesarczyk.dev</a> portfolio up and running 🚀</h1>\
+    <p><h3>Docs available at <a href="${port}/docs" >${port}/docs</a></h3></p>`
   }
 }
