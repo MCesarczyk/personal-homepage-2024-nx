6cbad239f0a956e0d1bd2839831a191d37d2028d
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-11-11 19:27:47 +0100

    test(user): add specs to service and controller of nest feature

---
+++ libs/nest/feature-user/src/lib/fixtures/users-array.fixture.ts
@@ -0,0 +1,20 @@
+export const usersArray = [
+  {
+    "id": "1",
+    "email": "john.doe@mail.com",
+    "name": "John Doe",
+    "password": "Password1!",
+    "occupation": "Software Engineer",
+    "introduction": "I am a geek who loves to code!",
+    "refreshToken": null
+  },
+  {
+    "id": "2",
+    "email": "mary.doe@mail.com",
+    "name": "Mary Doe",
+    "password": "Password2!",
+    "occupation": "UX Designer",
+    "introduction": "I love doing magic!",
+    "refreshToken": null
+  }
+]
--- libs/nest/feature-user/src/lib/nest-feature-user.controller.spec.ts
+++ libs/nest/feature-user/src/lib/nest-feature-user.controller.spec.ts
@@ -1,20 +1,54 @@
 import { Test } from '@nestjs/testing';
+import { NestDataAccessUserModule } from '@ph24/nest/data-access-user';
 import { NestFeatureUserController } from './nest-feature-user.controller';
 import { NestFeatureUserService } from './nest-feature-user.service';
+import { usersArray } from './fixtures/users-array.fixture';
+import { updatedUser } from './fixtures/updated-user.fixture';
 
 describe('NestFeatureUserController', () => {
   let controller: NestFeatureUserController;
 
   beforeEach(async () => {
     const module = await Test.createTestingModule({
-      providers: [NestFeatureUserService],
+      imports: [NestDataAccessUserModule],
       controllers: [NestFeatureUserController],
+      providers: [NestFeatureUserService],
     }).compile();
 
     controller = module.get(NestFeatureUserController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeTruthy();
+  afterAll(() => {
+    jest.resetAllMocks();
+  });
+
+  it('should return added user', async () => {
+    const result = usersArray[0];
+    jest.spyOn(controller, 'create').mockResolvedValue(result);
+    expect(await controller.create(usersArray[0])).toBe(result);
+  });
+
+  it('should return an array of users', async () => {
+    const result = usersArray;
+    jest.spyOn(controller, 'getAll').mockResolvedValue(result);
+    expect(await controller.getAll()).toBe(result);
+  });
+
+  it('should return a user by id', async () => {
+    const result = usersArray[0];
+    jest.spyOn(controller, 'getOne').mockResolvedValue(result);
+    expect(await controller.getOne('1')).toBe(result);
+  });
+
+  it('should return updated user', async () => {
+    const result = updatedUser;
+    jest.spyOn(controller, 'updateOne').mockResolvedValue(result);
+    expect(await controller.updateOne('1', usersArray[0])).toBe(result);
+  });
+
+  it('should return deleted user', async () => {
+    const result = usersArray[0];
+    jest.spyOn(controller, 'deleteOne').mockResolvedValue(result);
+    expect(await controller.deleteOne('1')).toBe(result);
   });
 });
