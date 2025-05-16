33a5fcfac50a432da23eb919bcffeaefb2a33b5f
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2025-05-17 01:30:57 +0200

    refactor(backend): remove obsolete inner modules

--- apps/backend/src/auth/entities/feedbackMessage.entity.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-
-export class FeedbackMessage {
-  @ApiProperty({
-    example: 'Congratulations! Operation successfully failed.',
-    description: 'Feedback message',
-  })
-  message: string;
-}
